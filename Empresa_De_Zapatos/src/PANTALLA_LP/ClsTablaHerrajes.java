0adbf58bc05a402ace1a8fe6f2b9c6ff7545539c
Unknown <david.requeta@opendeusto.es>
2019-05-11 12:51:11 +0200

    tabla materiales y herrajes

---
+++ Empresa_De_Zapatos/src/PANTALLA_LP/ClsTablaHerrajes.java
@@ -0,0 +1,118 @@
+package PANTALLA_LP;
+
+
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+import static COMUN.ClsConstantes.PROPIEDAD_HERRAJES_REFERENCIA;
+import static COMUN.ClsConstantes.PROPIEDAD_HERRAJES_DESCRIPCION;
+import static COMUN.ClsConstantes.PROPIEDAD_HERRAJES_PRECIO;
+import COMUN.ItfProperty;
+import LN.ClsGestorLN;
+
+/**
+ * Clase para formar la tabla Herrajes
+ * 
+ * @author Alex Salazar
+ * @author David Requeta
+ *
+ */
+public class ClsTablaHerrajes extends AbstractTableModel {
+	
+
+	/**
+	 * No se para que sirve esto.
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * Array Herrajes
+	 */
+	ArrayList<ItfProperty> HerrajesRecuperados;
+
+	private String[] NombreColumnas = { "Referencia", "Descripción" , "Precio"};
+	Object[][] data;
+
+	/**
+	 * 
+	 */
+	public ClsTablaHerrajes(ArrayList<ItfProperty> Herrajes) {
+		super();
+
+		int filas = Herrajes.size();
+		int cont;
+		data = new Object[filas][];
+		cont = 0;
+
+		for (ItfProperty b : Herrajes) {
+			Object[] a = { b.getIntegerProperty(PROPIEDAD_HERRAJES_REFERENCIA),
+					b.getStringProperty(PROPIEDAD_HERRAJES_DESCRIPCION),
+					b.getDoubleProperty(PROPIEDAD_HERRAJES_PRECIO),};
+			data[cont] = a;
+			cont++;
+		}
+	}
+
+	public Object[][] setData(ClsGestorLN ObjGestor) {
+		HerrajesRecuperados = ObjGestor.DameHerrajes();
+		int filas =HerrajesRecuperados.size();
+		int cont;
+		data = new Object[filas][];
+		cont = 0;
+
+		for (ItfProperty b : HerrajesRecuperados) {
+			Object[] a = { b.getIntegerProperty(PROPIEDAD_HERRAJES_REFERENCIA),
+					b.getStringProperty(PROPIEDAD_HERRAJES_DESCRIPCION),
+					b.getDoubleProperty(PROPIEDAD_HERRAJES_PRECIO),};
+			data[cont] = a;
+			cont++;
+		}
+		
+		return data;
+	}
+
+	public int getColumnCount() {
+		return NombreColumnas.length;
+	}
+
+	public int getRowCount() {
+		return data.length;
+	}
+
+	public String getColumnName(int col) {
+		return NombreColumnas[col];
+	}
+
+	public Object getValueAt(int row, int col) {
+		return data[row][col];
+	}
+
+	/*
+	 * JTable uses this method to determine the default renderer/ editor for each
+	 * cell. If we didn't implement this method, then the last column would contain
+	 * text ("true"/"false"), rather than a check box.
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public Class getColumnClass(int c) {
+		return getValueAt(0, c).getClass();
+	}
+
+	/*
+	 * Don't need to implement this method unless your table's editable.
+	 */
+	public boolean isCellEditable(int row, int col) {
+
+		return false;
+
+	}
+
+	/*
+	 * Don't need to implement this method unless your table's data can change.
+	 */
+	public void setValueAt(Object value, int row, int col) {
+
+		data[row][col] = value;
+		fireTableCellUpdated(row, col);
+
+	}
+
+}
---
+++ Empresa_De_Zapatos/src/PANTALLA_LP/ClsTablaMateriales.java
@@ -0,0 +1,119 @@
+package PANTALLA_LP;
+
+
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+import static COMUN.ClsConstantes.PROPIEDAD_MATERIALES_REFERENCIA;
+import static COMUN.ClsConstantes.PROPIEDAD_MATERIALES_DESCRIPCION;
+import static COMUN.ClsConstantes.PROPIEDAD_MATERIALES_PRECIO;
+import COMUN.ItfProperty;
+import LN.ClsGestorLN;
+
+/**
+ * Clase para formar la tabla Materiales
+ * 
+ * @author Alex Salazar
+ * @author David Requeta
+ *
+ */
+public class ClsTablaMateriales extends AbstractTableModel {
+	
+
+	/**
+	 * No se para que sirve esto.
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * Array Materiales
+	 */
+	ArrayList<ItfProperty> MaterialesRecuperados;
+
+	private String[] NombreColumnas = { "Referencia", "Descripción" , "Precio"};
+	Object[][] data;
+
+	/**
+	 * 
+	 */
+	public ClsTablaMateriales(ArrayList<ItfProperty> Materiales) {
+		super();
+
+		int filas = Materiales.size();
+		int cont;
+		data = new Object[filas][];
+		cont = 0;
+
+		for (ItfProperty b : Materiales) {
+			Object[] a = { b.getIntegerProperty(PROPIEDAD_MATERIALES_REFERENCIA),
+					b.getStringProperty(PROPIEDAD_MATERIALES_DESCRIPCION),
+					b.getDoubleProperty(PROPIEDAD_MATERIALES_PRECIO),};
+			data[cont] = a;
+			cont++;
+		}
+	}
+
+	public Object[][] setData(ClsGestorLN ObjGestor) {
+		MaterialesRecuperados = ObjGestor.DameMateriales();
+		int filas = MaterialesRecuperados.size();
+		int cont;
+		data = new Object[filas][];
+		cont = 0;
+
+		for (ItfProperty b : MaterialesRecuperados) {
+			Object[] a = { b.getIntegerProperty(PROPIEDAD_MATERIALES_REFERENCIA),
+					b.getStringProperty(PROPIEDAD_MATERIALES_DESCRIPCION),
+					b.getDoubleProperty(PROPIEDAD_MATERIALES_PRECIO),};
+			data[cont] = a;
+			cont++;
+		}
+		
+		return data;
+	}
+
+	public int getColumnCount() {
+		return NombreColumnas.length;
+	}
+
+	public int getRowCount() {
+		return data.length;
+	}
+
+	public String getColumnName(int col) {
+		return NombreColumnas[col];
+	}
+
+	public Object getValueAt(int row, int col) {
+		return data[row][col];
+	}
+
+	/*
+	 * JTable uses this method to determine the default renderer/ editor for each
+	 * cell. If we didn't implement this method, then the last column would contain
+	 * text ("true"/"false"), rather than a check box.
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public Class getColumnClass(int c) {
+		return getValueAt(0, c).getClass();
+	}
+
+	/*
+	 * Don't need to implement this method unless your table's editable.
+	 */
+	public boolean isCellEditable(int row, int col) {
+
+		return false;
+
+	}
+
+	/*
+	 * Don't need to implement this method unless your table's data can change.
+	 */
+	public void setValueAt(Object value, int row, int col) {
+
+		data[row][col] = value;
+		fireTableCellUpdated(row, col);
+
+	}
+
+
+}
